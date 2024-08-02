3265f92e6849548467e568013664f2fec964c738
Yashpambhar <bca2022yash1701@tnraocollege.org>
2024-08-02 22:05:10 +0530

    Update insertion.py

--- insertion.py
+++ insertion.py
@@ -10,3 +10,6 @@ def insertion_sort(arr):
 elements = [1, 4, 2, 8, 23]
 insertion_sort(elements)
 print("Sorted elements:", elements)
+
+output :-
+![pro7](https://github.com/user-attachments/assets/a42f7b59-7122-4736-8d00-32aafe7dd0e8)
